8374b8c923524f49ec6644c5f6169ea90413d2a8
Adrián Saul Polanco Ferrer <Adrian>
2023-12-10 09:36:23 -0400

    CRUD Terminado

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,32 +29,123 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            dataTable = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(408, 165);
+            button1.Location = new Point(390, 66);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "Create";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
+            // 
+            // dataTable
+            // 
+            dataTable.AutoSize = true;
+            dataTable.Location = new Point(12, -2);
+            dataTable.Name = "dataTable";
+            dataTable.Size = new Size(41, 20);
+            dataTable.TabIndex = 2;
+            dataTable.Text = "Data";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(390, 101);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 3;
+            button2.Text = "Read";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(390, 136);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 4;
+            button3.Text = "ReadById";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(373, 171);
+            button4.Name = "button4";
+            button4.Size = new Size(128, 29);
+            button4.TabIndex = 5;
+            button4.Text = "ReadByName";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(373, 215);
+            button5.Name = "button5";
+            button5.Size = new Size(127, 34);
+            button5.TabIndex = 6;
+            button5.Text = "ReadByPhone";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(390, 255);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 29);
+            button6.TabIndex = 7;
+            button6.Text = "Update";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(390, 290);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 29);
+            button7.TabIndex = 8;
+            button7.Text = "Delete";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(dataTable);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private Label dataTable;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
     }
 }
---
+++ InvalidContactException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaTelefonica
+{
+    internal class InvalidContactException: Exception
+    {
+        public InvalidContactException(string field, string requirementMessage): base($"Campo con formato invalido: {field}. Causa de la excepción: {requirementMessage}") {
+            
+        }
+    }
+}
