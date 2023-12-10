4482373f05e04d9edad7ee9e191feed9cd25ed5b
Adrián Saul Polanco Ferrer <Adrian>
2023-12-09 22:26:09 -0400

    Probando

---
+++ Database.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaTelefonica
+{
+    public class Database: DbContext
+    {
+        private static Database? instance;
+        private Database()
+        {
+
+        }
+
+        public static Database GetInstance()
+        {
+            if(instance == null) { instance = new Database(); }
+            return instance;
+        }
+    }
+}
