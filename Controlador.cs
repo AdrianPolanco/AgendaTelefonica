440abab7071b00347e4ce2c569fe904713296051
Adrián Saul Polanco Ferrer <Adrian>
2023-12-12 19:46:51 -0400

    Clase controlador añadida

---
+++ Controlador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaTelefonica
+{
+    public class Controlador
+    {
+        private List<TextBox> listaValores = new List<TextBox>();
+        public Controlador(TextBox nombre, TextBox apellido, TextBox empresa, TextBox telefono, TextBox correo)
+        {
+            listaValores.AddRange(new TextBox[] {nombre, apellido, empresa, telefono, correo});
+        }
+
+        public Contacto Capturar()
+        {
+            Contacto contacto = new Contacto()
+            {
+                Nombre = listaValores[0].Text,
+                Apellido = listaValores[1].Text,
+                Empresa = listaValores[2].Text,
+                Telefono = listaValores[3].Text,
+                Correo = listaValores[4].Text
+            };
+            contacto.Validar();
+            return contacto;
+        }
+    }
+}
